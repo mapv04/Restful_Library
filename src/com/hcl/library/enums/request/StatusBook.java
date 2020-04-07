6915c2debe8348b64b0663e6d3658a210ae87de1
Diana Siqueiros <DSV@ARS.local>
2020-04-07 14:07:45 -0500

    feature: add book resource and enum status class

---
+++ src/com/hcl/library/enums/request/StatusBook.java
@@ -0,0 +1,5 @@
+package com.hcl.library.enums.request;
+
+public enum StatusBook {
+	AVAILABLE, UNAVAILABLE, LOANED
+}
---
+++ src/com/hcl/library/request/Book.java
@@ -0,0 +1,20 @@
+package com.hcl.library.request;
+
+
+import com.hcl.library.enums.request.StatusBook;
+
+import lombok.Data;
+
+@Data
+public class Book {
+	private int id;
+	private String name;
+	private String isbn;
+	private String editorial;
+	private String edition;
+	private String category;
+	private String lenguage;
+	private String bookCover;
+	private StatusBook status;
+	
+}
